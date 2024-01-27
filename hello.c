e3ed59cd22a52b61ae5e8ab3555d50ebe06a96f2
SHUBHAM <shubhamgpt0442@gmail.com>
2024-01-27 22:52:54 +0530

    shubham

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+    /*char character;
+    printf("Enter a character: ");
+    scanf(" %c", &character);
+    if (character >= '0' && character <= '9') {
+        printf("%c is a digit.\n", character);
+    } else {
+        printf("%c is not a digit.\n", character);
+    }
+
+    return 0;
+}*/
+int main (){
+    int i;
+    for(i=1;i<=100;i++){
+    printf("hello \n");
+    }
+    return 0;
+}
+
+
+    
